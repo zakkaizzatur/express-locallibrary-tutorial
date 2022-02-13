f3f50cf5781ccf36a175496998d012eaad62ec13
zakkaizzatur <zakka.izzatur@gmail.com>
2022-02-13 16:58:52 +0700

    First version of application moved into Github

---
+++ app.js
@@ -0,0 +1,62 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog');
+var compression = require('compression');
+var helmet = require('helmet');
+
+var app = express();
+
+app.use(helmet());
+
+var mongoose = require('mongoose');
+
+var mongoDB = 'mongodb+srv://zakka:zakka@cluster0.ou0kq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+mongoose.connect(mongoDB, {useNewUrlParser: true , useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.use(compression()); //Compress all routes
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);  // Add catalog routes to middleware chain.
+
+module.exports = app;
---
+++ controllers/genreController.js
@@ -0,0 +1,174 @@
+var Genre = require('../models/genre');
+var Book = require('../models/book');
+var async = require('async');
+var debug = require('debug')('genre');
+
+const { body,validationResult } = require("express-validator");
+const { NativeError } = require('mongoose');
+
+// Display list of all Genre.
+exports.genre_list = function(req, res, next) {
+
+    Genre.find()
+      .sort([['name', 'ascending']])
+      .exec(function (err, list_genres) {
+        if (err) { return next(err); }
+        //Successful, so render
+        res.render('genre_list', { title: 'Genre List', genre_list: list_genres });
+      });
+  
+  };
+// Display detail page for a specific Genre.
+exports.genre_detail = function(req, res, next) {
+    async.parallel({
+        genre: function(callback) {
+            Genre.findById(req.params.id)
+                .exec(callback);
+        },
+
+        genre_books: function(callback) {
+            Book.find({'genre': req.params.id})
+                .exec(callback);
+        },
+
+    }, function(err, results) {
+        if(err) { return next(err); }
+        if (results.genre==null) {
+            var err = new Error('Genre not found');
+            err.status = 404;
+            return next(err);
+        }
+
+        res.render('genre_detail', { title: 'Genre Detail', genre: results.genre, genre_books: results.genre_books});
+    });
+};
+
+// Display Genre create form on GET.
+exports.genre_create_get = function(req, res, next) {
+    res.render('genre_form', { title: 'Create Genre' });
+};
+
+// Handle Genre create on POST.
+exports.genre_create_post = [
+    body('name', 'Genre name required').trim().isLength({ min: 1 }).escape(),
+
+    (req, res, next) => {
+        const errors = validationResult(req);
+
+        var genre = new Genre(
+            { name: req.body.name }
+        );
+
+        if (!errors.isEmpty()) {
+            res.render('genre_form', { title: 'Create Genre', genre: genre, errors: errors.array()});
+            return;
+        }
+        else {
+            Genre.findOne({ 'name': req.body.name })
+            .exec( function(err, found_genre) {
+                if (err) { return next(err); }
+
+                if (found_genre) {
+                    res.redirect(found_genre.url);
+                }
+                else {
+
+                    genre.save(function (err) {
+                        if (err) { return next(err); }
+                        res.redirect(genre.url);
+                    });
+                }
+            });
+        }
+    }
+];
+
+exports.genre_delete_get = function(req, res, next) {
+    async.parallel({
+        genre: function(callback) {
+            Genre.findById(req.params.id).exec(callback)
+        },
+        genre_books: function(callback) {
+            Book.find({ 'genre': req.params.id }).exec(callback)
+        },
+    }, function(err, results) {
+        if (err) { return next(err); }
+        if (results.genre==null) {
+            res.redirect('/catalog/genres');
+        }
+
+        res.render('genre_delete', { title: 'Delete Genre', genre: results.genre, genre_books: results.genre_books})
+    })
+}
+
+exports.genre_delete_post = function(req, res, next) {
+
+    async.parallel({
+        genre: function(callback) {
+            Genre.findById(req.body.genreid).exec(callback)
+        },
+        genre_books: function(callback) {
+            Book.find({ 'author': req.body.genreid }).exec(callback)
+        },
+    }, function(err, results) {
+        if (err) { return next(err); }
+        if (results.genre_books.length > 0) {
+            res.render('genre_delete', { title: 'Delete Genre', genre: results.genre, genre_books: results.genre_books});
+        }
+        else {
+            Genre.findByIdAndRemove(req.body.genreid, function deleteGenre(err) {
+                if (err) { return next(err); }
+                res.redirect('/catalog/genres')
+            })
+        }
+    });
+};
+
+// Display Genre update form on GET.
+exports.genre_update_get = function(req, res, next) {
+    async.parallel({
+        genre: function(callback) {
+            Genre.findById(req.params.id).exec(callback);
+        },
+    }, function(err, results) {
+        if (err) { return next(err); }
+        if (results.genre==null) {
+            var err = new Error('Genre not found')
+            err.status = 404;
+            return next(err);
+        }
+
+        res.render('genre_form', { title: 'Update Genre', genre:results.genre })
+    })
+}
+
+// Handle Genre update on POST.
+exports.genre_update_post = [
+
+    body('name', 'Genre name required').trim().isLength({ min: 1 }).escape(),
+
+    (req, res, next) => {
+        const errors = validationResult(req);
+
+        var genre = new Genre(
+            { name: req.body.name, _id:req.params.id}
+        );
+
+        if (!errors.isEmpty()) {
+            res.render('genre_form', { title: 'Create Genre', genre: genre, errors: errors.array()});
+            return;
+        }
+        else {
+            Genre.findByIdAndUpdate(req.params.id, genre, {}, function(err,thegenre){
+                if (err) { debug('update error:'+err); return next(err); }
+
+                res.redirect(thegenre.url);
+            });     
+        }
+    }
+
+
+
+]
+
+
