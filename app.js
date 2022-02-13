518d20c491c9bd42eb37fb7996c2fa922cba168c
zakkaizzatur <zakka.izzatur@gmail.com>
2022-02-13 17:05:09 +0700

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -16,7 +16,10 @@ app.use(helmet());
 
 var mongoose = require('mongoose');
 
-var mongoDB = 'mongodb+srv://zakka:zakka@cluster0.ou0kq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+// Set up mongoose connection
+var dev_db_url = 'mongodb+srv://zakka:zakka@cluster0.ou0kq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+
 mongoose.connect(mongoDB, {useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
